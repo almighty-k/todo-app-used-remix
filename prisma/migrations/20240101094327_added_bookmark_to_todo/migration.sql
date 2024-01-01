e070d4807c417a2c5e5174b7621635c62443601e
almighty-k <takada.chopin.work@gmail.com>
2024-01-01 18:45:35 +0900

    feat: Add bookmark field to Todo model

---
+++ prisma/migrations/20240101094327_added_bookmark_to_todo/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "progress" TEXT NOT NULL,
+    "bookmark" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Todo" ("createdAt", "id", "progress", "title", "updatedAt", "userId") SELECT "createdAt", "id", "progress", "title", "updatedAt", "userId" FROM "Todo";
+DROP TABLE "Todo";
+ALTER TABLE "new_Todo" RENAME TO "Todo";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
