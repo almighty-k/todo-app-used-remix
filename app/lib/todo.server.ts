d9af97d0f8c17a5f37c4a9075ac20da48d5eca38
almighty-k <takada.chopin.work@gmail.com>
2024-01-01 22:26:03 +0900

    feat: CreateTodoSchema and createTodo function

--- app/lib/todo.server.ts
+++ app/lib/todo.server.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { prisma } from "./db.server";
 
 export async function getTodos({
@@ -11,6 +12,30 @@ export async function getTodos({
     where: {
       userId,
       progress,
+    },
+  });
+}
+
+export const CreateTodoSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "タイトルは必須です。" })
+    .max(20, { message: "タイトルは20文字以下で入力してください。" }),
+});
+
+export async function createTodo({
+  userId,
+  title,
+}: { userId: number } & z.infer<typeof CreateTodoSchema>) {
+  return await prisma.todo.create({
+    data: {
+      title,
+      progress: "incomplete",
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
     },
   });
 }
