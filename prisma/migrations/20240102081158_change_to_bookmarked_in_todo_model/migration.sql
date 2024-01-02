4da50c2849bdc9e2083804d186d0e50e8602877a
almighty-k <takada.chopin.work@gmail.com>
2024-01-02 17:23:27 +0900

    fix: Change bookmark column to bookmarked in Todo model

---
+++ prisma/migrations/20240102081158_change_to_bookmarked_in_todo_model/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `bookmark` on the `Todo` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "progress" TEXT NOT NULL,
+    "bookmarked" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Todo" ("createdAt", "id", "progress", "title", "updatedAt", "userId") SELECT "createdAt", "id", "progress", "title", "updatedAt", "userId" FROM "Todo";
+DROP TABLE "Todo";
+ALTER TABLE "new_Todo" RENAME TO "Todo";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
