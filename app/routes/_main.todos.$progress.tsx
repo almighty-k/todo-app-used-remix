e3ce7dc3736a2d200ff344f890af0ece4a9c8604
almighty-k <takada.chopin.work@gmail.com>
2024-01-02 12:02:06 +0900

    feat: Add deleting feature on todos page

--- app/routes/_main.todos.$progress.tsx
+++ app/routes/_main.todos.$progress.tsx
@@ -1,19 +1,47 @@
-import { json, type LoaderFunctionArgs } from "@remix-run/node";
-import { Outlet, useLoaderData, useNavigation } from "@remix-run/react";
-import { Text, Table, Rating, ActionIcon, TableTbody } from "@mantine/core";
+import {
+  ActionFunctionArgs,
+  json,
+  redirect,
+  type LoaderFunctionArgs,
+} from "@remix-run/node";
+import {
+  Form,
+  Outlet,
+  useLoaderData,
+  useNavigate,
+  useNavigation,
+  useParams,
+  useSearchParams,
+} from "@remix-run/react";
+import {
+  Text,
+  Table,
+  Rating,
+  ActionIcon,
+  TableTbody,
+  Modal,
+  Button,
+  Stack,
+} from "@mantine/core";
 import invariant from "tiny-invariant";
 
 import { authenticator } from "../lib/auth.server";
 import { EditIcon, DeleteIcon } from "../components/icons";
 import { TodosProgressBadge } from "../components/todos";
 import { CenterLoader } from "../components/loader";
-import { getTodos } from "../lib/todo.server";
+import { deleteTodo, getTodos } from "../lib/todo.server";
 import { ERROR_MESSAGES } from "../utils";
+import {
+  SUCCESS_MESSAGE_KEY,
+  commitSession,
+  getSession,
+} from "../lib/session.server";
 
 export default function TodosByStatus() {
   const { todos } = useLoaderData<typeof loader>();
 
   const navigation = useNavigation();
+
   if (navigation.state === "loading")
     return (
       <>
@@ -28,7 +56,6 @@ export default function TodosByStatus() {
         <Text my="sm" size="sm" ta="center">
           該当するTODOはありません。
         </Text>
-
         <Outlet />
       </>
     );
@@ -56,17 +83,66 @@ export default function TodosByStatus() {
                 </ActionIcon>
               </Table.Td>
               <Table.Td w="3rem">
-                <ActionIcon color="red" variant="light">
-                  <DeleteIcon />
-                </ActionIcon>
+                {/* クエリパラメータを使用し、モーダル開閉を制御する */}
+                <Form replace>
+                  <ActionIcon
+                    type="submit"
+                    name="deletedId"
+                    color="red"
+                    variant="light"
+                    value={todo.id}
+                  >
+                    <DeleteIcon />
+                  </ActionIcon>
+                  <input
+                    type="hidden"
+                    name="deletedTitle"
+                    defaultValue={todo.title}
+                  />
+                </Form>
               </Table.Td>
             </Table.Tr>
           ))}
         </TableTbody>
       </Table>
 
+      <DeleteConfirmModal />
       <Outlet />
     </>
+  );
+}
+
+function DeleteConfirmModal() {
+  const { progress } = useParams();
+  invariant(progress, ERROR_MESSAGES.invalidParam);
+
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  return (
+    <Modal
+      opened={!!searchParams.get("deletedId")}
+      onClose={() => navigate(`/todos/${progress}`, { replace: true })}
+      title="削除確認"
+    >
+      <Stack
+        renderRoot={(props) => <Form method="post" replace {...props} />}
+        gap="sm"
+      >
+        <Text>
+          {searchParams.get("deletedTitle")}を削除します。よろしいですか？
+        </Text>
+        <Button
+          type="submit"
+          name="intent"
+          value="delete"
+          ml="auto"
+          color="red"
+        >
+          削除
+        </Button>
+      </Stack>
+    </Modal>
   );
 }
 
@@ -81,4 +157,32 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
   return json({
     todos: await getTodos({ userId: user.id, progress }),
   });
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+
+  switch (formData.get("intent")) {
+    case "delete": {
+      const url = new URL(request.url);
+
+      const deletedId = Number(url.searchParams.get("deletedId"));
+      const deletedTodo = await deleteTodo({ id: deletedId });
+
+      const session = await getSession(request.headers.get("Cookie"));
+      session.flash(
+        SUCCESS_MESSAGE_KEY,
+        `${deletedTodo.title}を削除しました。`
+      );
+
+      return redirect(url.pathname, {
+        headers: {
+          "Set-Cookie": await commitSession(session),
+        },
+      });
+    }
+    default: {
+      throw Error(ERROR_MESSAGES.unexpected);
+    }
+  }
 }
